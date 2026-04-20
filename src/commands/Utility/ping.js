3bc01d96d51933acb888992f793a792993c1fa1b
iamisolo <vijetabatham84@gmail.com>
2026-04-20 21:42:28 +0530

    Add slash command support to ping command

--- src/commands/Utility/ping.js
+++ src/commands/Utility/ping.js
@@ -1,5 +1,11 @@
+import { SlashCommandBuilder } from 'discord.js';
+
 export default {
   name: 'ping',
+
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Ping command'),
 
   async execute(interaction) {
     await interaction.reply('Pong (slash)');
